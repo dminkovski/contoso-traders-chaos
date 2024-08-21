c934119d38255035eee5d427e75c11b342322d7d
David Minkovski <dminkovski@microsoft.com>
2024-08-21 11:55:56 +0200

    Removed local settings from .NET

--- src/app/ContosoTraders.Api.Core/Constants/CosmosConstants.cs
+++ src/app/ContosoTraders.Api.Core/Constants/CosmosConstants.cs
@@ -2,10 +2,10 @@
 
 public class CosmosConstants
 {
-    public const string DatabaseNameCarts = "dm1992contosocosmos-cartsdb";
+    public const string DatabaseNameCarts = "cartsdb";
     public const string DatabaseNameStocks = "stocksdb";
 
     // keep the rest of the properties in alphabetical order
-    public const string ContainerNameCarts = "dm1992contosocosmos-cartsdbc";
+    public const string ContainerNameCarts = "cartsdbc";
     public const string ContainerNameStocks = "stocks";
 }
--- src/app/ContosoTraders.Api.Core/DependencyInjection.cs
+++ src/app/ContosoTraders.Api.Core/DependencyInjection.cs
@@ -38,15 +38,10 @@ public class DependencyInjection : FunctionsStartup
     {
         var builder = WebApplication.CreateBuilder();
 
-        var credentialOptions = new DefaultAzureCredentialOptions
-        {
-            TenantId = "16b3c013-d300-468d-ac64-7eda0820b6d3"
-        };
-
         // if (builder.Environment.IsDevelopment())
         builder.Configuration.AddAzureKeyVault(
                 new Uri(builder.Configuration["KeyVaultEndpoint"]),
-                new DefaultAzureCredential(credentialOptions));
+                new DefaultAzureCredential());
         // else
         //     builder.Configuration.AddAzureKeyVault(
         //         new Uri(builder.Configuration["KeyVaultEndpoint"]),
