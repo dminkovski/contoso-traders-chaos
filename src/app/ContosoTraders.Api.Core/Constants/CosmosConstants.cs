e6a516d16afd019dfa1e9b2dfce0ce1814ee7cb0
David Minkovski <dminkovski@microsoft.com>
2024-08-21 12:22:23 +0200

    Constants Cosmos

--- src/app/ContosoTraders.Api.Core/Constants/CosmosConstants.cs
+++ src/app/ContosoTraders.Api.Core/Constants/CosmosConstants.cs
@@ -6,6 +6,6 @@ public class CosmosConstants
     public const string DatabaseNameStocks = "stocksdb";
 
     // keep the rest of the properties in alphabetical order
-    public const string ContainerNameCarts = "cartsdbc";
+    public const string ContainerNameCarts = "carts";
     public const string ContainerNameStocks = "stocks";
 }
